5be3ad8614cc58d73cafd259b5e4817f581e0a04
Amaan <amaanpathan5@gmail.com>
2020-10-09 21:23:53 +0530

    Upate: shows percentage, Can share file from any folder

--- client/client.py
+++ client/client.py
@@ -8,37 +8,37 @@ c.connect(('', 4444))
 
 class Client:
 
-    def __init__(self, client):
-        self.file = None
-        self.path = ''
-        self.client = client
-        self.file_paths = []
+    def __init__(self):
+        self.send_file = None
+        self.send_path = ''
 
-    def start(self):
+    def __get_names(self):
+        file_paths = []
         for file_number in range(int(input('Enter the number of files to be send: '))):
-            self.file_paths.append(input(f"Enter the file path of {file_number+1} file: "))
+            file_paths.append(input(f"Enter the file path of {file_number + 1} file: "))
+        return file_paths
 
-    def send(self):
-        self.start()
-        for file in self.file_paths:
+    def send(self, client):
+        file_paths = self.__get_names()
+        for file in file_paths:
             print("Started sending: ", file)
-            self.file = open(file, 'rb')
-            self.path = self.to_encoded_form(file)
-            send_file_name = self.to_encoded_form(str({'header': f'{len(self.path):<12}'}))
+            self.send_file = open(file, 'rb')
+            self.send_path = self.__to_encoded_form(file.split('/')[-1] if '/' in file else file)
 
-            send_file_size = self.to_encoded_form(str({'header': f'{os.stat(self.path).st_size:<12}'}))
+            send_file_size = self.__to_encoded_form(str({'header': f'{os.stat(file).st_size:<12}'}))
+            send_file_name = self.__to_encoded_form(str({'header': f'{len(self.send_path):<12}'}))
 
-            self.client.send(send_file_name)
-            self.client.send(self.path)
-            self.client.send(send_file_size)
-            byte = self.file.read(26)
+            client.send(send_file_name)
+            client.send(self.send_path)
+            client.send(send_file_size)
+            byte = self.send_file.read(26)
             while byte:
-                self.client.send(byte)
-                byte = self.file.read(26)
-            self.file.close()
+                client.send(byte)
+                byte = self.send_file.read(26)
+            self.send_file.close()
             print('done Sending: ', file)
 
-    def to_encoded_form(self, string):
+    def __to_encoded_form(self, string):
         return string.encode('utf-8')
 
 
@@ -63,5 +63,5 @@ class Client:
 #             c.close()
 #             break
 
-threading.Thread(target=Client(c).send).start()
+threading.Thread(target=Client().send, args=[c]).start()
 # threading.Thread(target=receive).start()
--- server/server.py
+++ server/server.py
@@ -10,57 +10,67 @@ s.listen()
 class Server:
 
     def __init__(self):
-        self.name_flag = True
-        self.received_bytes = 26
-        self.path = ''
-        self.second_msg_flag = True
-        self.first_msg_flag = True
-        self.name_len = None
-        self.received_data = None
-        self.msg_len = None
-        self.file = None
+        self.listen_name_flag = True
+        self.listen_received_bytes = 26
+        self.listen_total_file_length = 0
+        self.listen_path = ''
+        self.listen_second_msg_flag = True
+        self.listen_first_msg_flag = True
+        self.listen_name_len = None
+        self.listen_received_data = None
+        self.listen_msg_len = None
+        self.listen_file = None
 
-    def listen(self, client, addr):
+    def listen(self, client):
         while True:
-            self.receive_data(client, self.received_bytes)
-            if self.received_data: self.first_msg_flag_functionality() if self.first_msg_flag else self.setting_path_and_file_data(client)
+            self.__receive_data(client, self.listen_received_bytes)
+            if self.listen_received_data: self.__first_msg_flag_functionality() if self.listen_first_msg_flag else self.__setting_path_and_file_data(client)
 
-    def receive_data(self, client, bytes_to_receive):
-        self.received_data = client.recv(bytes_to_receive)
-        return self.received_data
+    def __receive_data(self, client, bytes_to_receive):
+        self.listen_received_data = client.recv(bytes_to_receive)
+        return self.listen_received_data
 
-    def setting_path_and_file_data(self, client):
-        self.set_path_of_file(client) if self.name_flag else self.second_msg_flag_functionality() if self.second_msg_flag else self.write_in_file(client)
+    def __setting_path_and_file_data(self, client):
+        self.__set_path_of_file(client) if self.listen_name_flag else self.__second_msg_flag_functionality() if self.listen_second_msg_flag else self.__write_in_file(client)
 
-    def set_path_of_file(self, client):
-        self.path += self.received_data.decode('utf-8')
-        self.name_len -= self.received_bytes
-        if self.name_len < self.received_bytes:
-            if self.name_len > 0: self.path += self.receive_data(client, self.name_len).decode('utf-8')
-            self.name_flag = not self.name_flag
-            self.received_bytes = 26
-            self.file = open(self.path, 'wb')
-            print("Receiving: ", self.path)
+    def __set_path_of_file(self, client):
+        self.listen_path += self.listen_received_data.decode('utf-8')
+        self.listen_name_len -= self.listen_received_bytes
+        if self.listen_name_len < self.listen_received_bytes:
+            if self.listen_name_len > 0: self.listen_path += self.__receive_data(client, self.listen_name_len).decode('utf-8')
+            self.listen_name_flag = not self.listen_name_flag
+            self.listen_received_bytes = 26
+            self.listen_file = open(self.listen_path, 'wb')
+            print("Receiving: ", self.listen_path)
 
-    def write_in_file(self, client):
-        self.file.write(self.received_data)
-        if len(self.received_data) != 26 and self.msg_len > 26: self.msg_len += abs(26 - len(self.received_data))
-        self.msg_len -= self.received_bytes
-        if self.msg_len < self.received_bytes:
-            if self.msg_len > 0: self.file.write(self.receive_data(client, self.msg_len))
-            self.file.close()
-            print('Received: ', self.file.name)
+    def __write_in_file(self, client):
+        self.listen_file.write(self.listen_received_data)
+        if len(self.listen_received_data) != 26 and self.listen_msg_len > 26: self.listen_msg_len += abs(26 - len(self.listen_received_data))
+        self.listen_msg_len -= self.listen_received_bytes
+        print('completed: ', round(100 - ((self.listen_msg_len / self.listen_total_file_length) * 100)), '%')
+        if self.listen_msg_len < self.listen_received_bytes:
+            if self.listen_msg_len > 0: self.listen_file.write(self.__receive_data(client, self.listen_msg_len))
+            print('completed:  100 %')
+            self.listen_file.close()
+            print('Received: ', self.listen_file.name)
             self.__init__()
 
-    def first_msg_flag_functionality(self):
-        self.first_msg_flag = not self.first_msg_flag
-        self.name_len = int(eval(self.received_data.decode('utf-8'))['header'])
-        self.received_bytes = self.name_len if self.name_len < self.received_bytes else self.received_bytes
+    def __first_msg_flag_functionality(self):
+        self.listen_first_msg_flag = not self.listen_first_msg_flag
+        self.listen_name_len = int(eval(self.listen_received_data.decode('utf-8'))['header'])
+        self.listen_received_bytes = self.listen_name_len if self.listen_name_len < self.listen_received_bytes else self.listen_received_bytes
 
-    def second_msg_flag_functionality(self):
-        self.second_msg_flag = not self.second_msg_flag
-        self.msg_len = int(eval(self.received_data.decode('utf-8'))['header'])
-        self.received_bytes = self.msg_len if self.msg_len < self.received_bytes else self.received_bytes
+    def __second_msg_flag_functionality(self):
+        self.listen_second_msg_flag = not self.listen_second_msg_flag
+        self.listen_msg_len = int(eval(self.listen_received_data.decode('utf-8'))['header'])
+        self.listen_total_file_length = self.listen_msg_len
+        self.listen_received_bytes = self.listen_msg_len if self.listen_msg_len < self.listen_received_bytes else self.listen_received_bytes
+
+
+
+
+
+
 
 
 # def send(c, addr):
@@ -77,9 +87,8 @@ class Server:
 #         connection = False
 #     print('done')
 
-
 while True:
     c, addr = s.accept()
     print(addr, c)
-    threading.Thread(target=Server().listen, args=(c, addr)).start()
+    threading.Thread(target=Server().listen, args=[c]).start()
     # threading.Thread(target=send, args=(c, addr)).start()
